3d10a831b6a2df394350b98a48dcf9b80fe662ce
GenkiOsawa <taiiku39@gmail.com>
2021-05-22 18:10:26 +0900

    many heroes display and edit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id:11, name: 'Naruto' },
+  { id:12, name: 'Bad man' },
+  { id: 13, name: 'Super man' },
+  { id: 14, name: 'annpan man' },
+  { id: 15, name: 'Luffy' },
+  { id: 16, name: 'zoro' },
+  { id: 17, name: 'conann' },
+  { id: 18, name: 'amuro' },
+  { id: 19, name: 'odai wo kudasai' },
+  { id: 20, name: 'last' }
+]
